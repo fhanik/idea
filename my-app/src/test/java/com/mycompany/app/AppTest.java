b6c2fe988f9e113430f2d8722739d2f180352525
Filip Hanik <fhanik@gopivotal.com>
2014-02-17 08:56:35 -0700

    simple idea/intellij test failure

---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,39 @@
+package com.mycompany.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        String property = System.getProperty("spring.profiles.active");
+        assertTrue(property == null || property.trim().length() == 0);
+    }
+}
